9e24d6aa56820b5dede36f1c07dc6b7e696070f5
Alexis Salnikoff <salnikoff.a@gmail.com>
2019-12-11 12:33:44 +0100

    add error management

--- src/server.js
+++ src/server.js
@@ -4,10 +4,13 @@ const app = express();
 const bodyParser = require("body-parser");
 const routesManager = require("@features/routes");
 const { apiPort } = require("@config/config.json");
+const { errorsHandling } = require("@middlewares/error");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(routesManager);
+
+app.use(errorsHandling);
 
 const server = app.listen(apiPort, () => {
   console.log(`Server is listening on ${server.address().port}`);
